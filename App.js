d1f77a81e1f055276e40b984db100d6f9a7c8578
Vitaliy <270vitalik270@gmail.com>
2021-12-16 22:19:16 +0200

    added navigation

--- App.js
+++ App.js
@@ -1,167 +1,39 @@
 import React from 'react';
-import { SectionList } from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-import styled from 'styled-components/native'
-import { Appointment, SectionTitle } from './components'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import {HomeScreen, PatientDetailsScreen} from "./screens";
 
-const DATA = [
-  {
-    title: "11 september",
-    data: [
-      {
-        id: 1,
-        time: '12:00',
-        active: true,
-        diagnosis: 'some illness',
-        user: {
-          fullname: 'patient name1',
-          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
-        }
-      },
-      {
-        id: 2,
-        time: '13:00',
-        diagnosis: 'another illness',
-        user: {
-          fullname: 'patient name2',
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
-        }
-      },
-    ]
+const Stack = createNativeStackNavigator();
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#fff',
   },
-  {
-    title: "12 september",
-    data: [
-      {
-        id: 3,
-        time: '12:00',
-        active: true,
-        diagnosis: 'some illness',
-        user: {
-          fullname: 'patient name1',
-          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
-        }
-      },
-      {
-        id: 4,
-        time: '13:00',
-        diagnosis: 'another illness',
-        user: {
-          fullname: 'patient name2',
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
-        }
-      },
-    ]
-  },
-  {
-    title: "13 september",
-    data: [
-      {
-        id: 5,
-        time: '12:00',
-        active: true,
-        diagnosis: 'some illness',
-        user: {
-          fullname: 'patient name1',
-          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
-        }
-      },
-      {
-        id: 6,
-        time: '13:00',
-        diagnosis: 'another illness!!!',
-        user: {
-          fullname: 'patient name2',
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
-        }
-      },
-    ]
-  },
-  {
-    title: "14 september",
-    data: [
-      {
-        id: 7,
-        time: '12:00',
-        active: true,
-        diagnosis: 'some illness',
-        user: {
-          fullname: 'patient name1',
-          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
-        }
-      },
-      {
-        id: 8,
-        time: '13:00',
-        diagnosis: 'another illness',
-        user: {
-          fullname: 'patient name2',
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
-        }
-      },
-    ]
-  },
-  {
-    title: "15 september",
-    data: [
-      {
-        id: 9,
-        time: '12:00',
-        active: true,
-        diagnosis: 'some illness',
-        user: {
-          fullname: 'patient name1',
-          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
-        }
-      },
-      {
-        id: 10,
-        time: '13:00',
-        diagnosis: 'another illness',
-        user: {
-          fullname: 'patient name2',
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
-        }
-      },
-    ]
-  },
-];
+};
 
 export default function App() {
+
   return (
-    <Container>
-      <SectionList
-        sections={DATA}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Appointment {...item} />}
-        renderSectionHeader={({ section: { title } }) => (
-          <SectionTitle title={title} />
-        )}
-      />
-      <PlusButton>
-        <Ionicons name={'ios-add'} size={36} color={'white'} />
-      </PlusButton>
-    </Container>
+      <NavigationContainer
+        theme={navTheme}
+      >
+        <Stack.Navigator
+          initialRouteName="Patients"
+          screenOptions={{
+            headerTintColor: '#2A86FF',
+            headerShadowVisible: false,
+          }}
+        >
+          <Stack.Screen
+            name="Patients"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="Patient card"
+            component={PatientDetailsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
-
-const PlusButton = styled.TouchableOpacity`
-  align-items: center;
-  justify-content: center;
-  border-radius: 50px;
-  width: 64px;
-  height: 64px;
-  background: #2A86FF;
-  position: absolute;
-  right: 25px;
-  bottom: 25px;
-  shadow-color: #2A86FF;
-  shadow-opacity: 1;
-  shadow-radius: 15;
-  elevation: 16;
-`
-
-const Container = styled.View`
-  flex: 1;
-  margin-top: 50px;
-`
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,182 @@
+import React from 'react';
+import { SectionList } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import styled from 'styled-components/native'
+import { Appointment, SectionTitle } from '../components'
+
+const DATA = [
+  {
+    title: "11 september",
+    data: [
+      {
+        id: 1,
+        time: '12:00',
+        active: true,
+        diagnosis: 'some illness',
+        user: {
+          fullname: 'patient name1',
+          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
+        }
+      },
+      {
+        id: 2,
+        time: '13:00',
+        diagnosis: 'another illness',
+        user: {
+          fullname: 'patient name2',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
+        }
+      },
+    ]
+  },
+  {
+    title: "12 september",
+    data: [
+      {
+        id: 3,
+        time: '12:00',
+        active: true,
+        diagnosis: 'some illness',
+        user: {
+          fullname: 'patient name1',
+          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
+        }
+      },
+      {
+        id: 4,
+        time: '13:00',
+        diagnosis: 'another illness',
+        user: {
+          fullname: 'patient name2',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
+        }
+      },
+    ]
+  },
+  {
+    title: "13 september",
+    data: [
+      {
+        id: 5,
+        time: '12:00',
+        active: true,
+        diagnosis: 'some illness',
+        user: {
+          fullname: 'patient name1',
+          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
+        }
+      },
+      {
+        id: 6,
+        time: '13:00',
+        diagnosis: 'another illness!!!',
+        user: {
+          fullname: 'patient name2',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
+        }
+      },
+    ]
+  },
+  {
+    title: "14 september",
+    data: [
+      {
+        id: 7,
+        time: '12:00',
+        active: true,
+        diagnosis: 'some illness',
+        user: {
+          fullname: 'patient name1',
+          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
+        }
+      },
+      {
+        id: 8,
+        time: '13:00',
+        diagnosis: 'another illness',
+        user: {
+          fullname: 'patient name2',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
+        }
+      },
+    ]
+  },
+  {
+    title: "15 september",
+    data: [
+      {
+        id: 9,
+        time: '12:00',
+        active: true,
+        diagnosis: 'some illness',
+        user: {
+          fullname: 'patient name1',
+          avatar: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'
+        }
+      },
+      {
+        id: 10,
+        time: '13:00',
+        diagnosis: 'another illness',
+        user: {
+          fullname: 'patient name2',
+          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcemOrw8mSjbGRWPILUjf-J-VArHi54RO7A&usqp=CAU'
+        }
+      },
+    ]
+  },
+];
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  // React.useLayoutEffect(() => {
+  //   navigation.setOptions({
+  //     title: 'Patients',
+  //     headerTintColor: '#2A86FF',
+  //     headerStyle: {
+  //       elevation: 1,
+  //       shadowOpacity: 0
+  //     }
+  //   });
+  // }, [navigation]);
+
+  return (
+    <Container>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Appointment {...item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <SectionTitle title={title} />
+        )}
+      />
+      <PlusButton>
+        <Ionicons name={'ios-add'} size={36} color={'white'} />
+      </PlusButton>
+    </Container>
+  );
+}
+
+export default HomeScreen
+
+const PlusButton = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  border-radius: 50px;
+  width: 64px;
+  height: 64px;
+  background: #2A86FF;
+  position: absolute;
+  right: 25px;
+  bottom: 25px;
+  shadow-color: #2A86FF;
+  shadow-opacity: 1;
+  shadow-radius: 15px;
+  elevation: 16;
+`
+
+const Container = styled.View`
+  flex: 1;
+`
